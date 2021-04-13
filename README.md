3b36f35718dda1dd10d8aa22dceba992043a7754
疯狂创客圈 <304008029@qq.com>
2021-04-13 09:29:58 +0800

    优化代码

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 ## 学习说明
 
-本软件作为分布式Java开发的练习和实战入手项目，实际上，和 生产项目在架构上差不多。
-只是在细节上处理的内容，生产项目更加丰富和复杂。
 
 本项目的配套材料，请参考：
- 
-- 配套图书 [《Java高并发核心编程（卷2）》一书 ](https://www.cnblogs.com/crazymakercircle/p/9904544.html)
-- 配套视频：[《从菜鸟大神-Java高并发核心编程》视频](https://www.cnblogs.com/crazymakercircle/p/14434174.html) 部分视频
+- [《Java高并发核心编程（卷2）》一书 ](https://www.cnblogs.com/crazymakercircle/p/9904544.html)
+- 配套视频：[《从菜鸟大神-Java高并发核心编程》视频](https://www.cnblogs.com/crazymakercircle/p/9904544.html)
+- 总目录：[**疯狂创客圈 高并发 总目录**](https://www.cnblogs.com/crazymakercircle/p/9904544.html) 
 
 ## NIO、Netty、Java高并发的发烧友圈子： [疯狂创客圈](https://www.cnblogs.com/crazymakercircle/p/9904544.html) 
- [被誉为全网唯一不吹牛逼，只交流技术的Java工程师圈子。](https://www.cnblogs.com/crazymakercircle/p/9904544.html) 
+ 
+ > [被誉为全网唯一不吹牛逼，只交流技术的Java工程师圈子。](https://www.cnblogs.com/crazymakercircle/p/9904544.html) 
---
+++ thread-demos/src/main/java/com/crazymakercircle/mutithread/basic/threadlocal/SimpleDemo.java
@@ -0,0 +1,29 @@
+package com.crazymakercircle.mutithread.basic.threadlocal;
+import com.crazymakercircle.util.ThreadUtil;
+public class SimpleDemo {
+    static class DemoClass {
+        private ThreadLocal<Integer> i = new ThreadLocal<Integer>() {
+            Integer integer;
+            @Override
+            protected Integer initialValue() {
+                return 0;
+            }
+        };
+        public Integer get() {
+            return i.get();
+        }
+        public void set(Integer integer) {
+            i.set(i.get() + integer);
+        }
+    }
+    public static void main(String[] args) {
+        DemoClass demoClass = new DemoClass();
+        for (int i = 1; i <= 100; i++) {
+            new Thread(() -> {
+                for (int j = 1; j <= 100; j++) demoClass.set(j);
+                System.out.println("demoClass.get() = " + demoClass.get());
+            }).start();
+        }
+        ThreadUtil.sleepSeconds(Integer.MAX_VALUE);
+    }
+}
