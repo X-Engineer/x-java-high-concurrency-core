a8a557b1a0f3c8329c97a057287783dee3ad26de
nzcer <ningzhicheng1215@qq.com>
2023-03-09 19:00:18 +0800

    refactor: 完成 1.4 线程的核心原理
    线程的调度与时间片，优先级，生命周期，jstack

--- thread-demos/src/main/java/com/crazymakercircle/mutithread/basic/use/PriorityDemo.java
+++ thread-demos/src/main/java/com/crazymakercircle/mutithread/basic/use/PriorityDemo.java
@@ -2,7 +2,10 @@ package com.crazymakercircle.mutithread.basic.use;
 
 import com.crazymakercircle.util.Print;
 
+import java.util.concurrent.locks.LockSupport;
+
 /**
+ * Java中使用抢占式调度模型进行线程调度。priority实例属性的优先级越高，线程获得CPU时间片的机会就越多，但也不是绝对的。
  * Created by 尼恩@疯狂创客圈.
  */
 
@@ -26,7 +29,7 @@ public class PriorityDemo {
         }
     }
 
-    public static void main(String args[]) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
         PrioritySetThread[] threads = new PrioritySetThread[10];
         for (int i = 0; i < threads.length; i++) {
@@ -42,6 +45,10 @@ public class PriorityDemo {
 
         Thread.sleep(SLEEP_GAP);
 
+        /**
+         * Thread类的stop()实例方法是一个过时的方法，也是一个不安全的方法。这里的安全指的是系统资源（文件、网络连接等）的安全——stop()实例方法可能导致资源状态不一致，
+         * 或者说资源出现问题时很难定位。在实际开发过程中，不建议使用stop()实例方法。
+         */
         for (int i = 0; i < threads.length; i++) {
             threads[i].stop();
         }
