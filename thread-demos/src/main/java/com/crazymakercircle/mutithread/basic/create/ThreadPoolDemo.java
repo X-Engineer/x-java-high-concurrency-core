4abe9853b52c3d85f6dbf2854f76361d789e77cd
nzcer <ningzhicheng1215@qq.com>
2023-04-05 10:03:02 +0800

    refactor: 完成 1.7 确定线程池的线程数
    - 为 IO 密集型任务确定线程数
    - 为 CPU 密集型任务确定线程数
    - 为混合型任务确定线程数

--- thread-demos/src/main/java/com/crazymakercircle/mutithread/basic/create/ThreadPoolDemo.java
+++ thread-demos/src/main/java/com/crazymakercircle/mutithread/basic/create/ThreadPoolDemo.java
@@ -13,6 +13,14 @@ import static com.crazymakercircle.util.ThreadUtil.*;
 
 /**
  * Created by 尼恩@疯狂创客圈.
+ * 线程适用的场景一般是：存在相当比例非CPU耗时操作，如IO、网络操作，需要尽量提高并行化比率以提升CPU的利用率
+ * 确定线程池的线程数
+ * IO 密集型任务：线程数通常配置为 CPU 核数 * 2
+ * CPU 密集型任务：线程数通常配置为 CPU 核数
+ * 混合型任务：最佳线程数目 = (线程等待时间/线程CPU时间 + 1) * CPU核数
+ * 举一个例子：比如在Web服务器处理HTTP请求时，假设平均线程CPU运行时间为100毫秒，而线程等待时间（比如包括DB操作、RPC操作、缓存操作等）为900毫秒，
+ * 如果CPU核数为8，那么根据上面这个公式，估算如下：
+ *      (900毫秒 + 100毫秒) / 100毫秒 * 8 = 10 * 8 = 80
  */
 
 public class ThreadPoolDemo {
@@ -21,7 +29,7 @@ public class ThreadPoolDemo {
 
     static int threadNo = 1;
 
-    static class RunTarget implements Runnable  //① 实现Runnable接口
+    class RunTarget implements Runnable  //① 实现Runnable接口
     {
         public void run()  //② 在这些写业务逻辑
         {
@@ -37,16 +45,7 @@ public class ThreadPoolDemo {
     public void testIoIntenseTargetThreadPool() {
         ThreadPoolExecutor pool = ThreadUtil.getIoIntenseTargetThreadPool();
         for (int i = 0; i < 2; i++) {
-            pool.submit(new Runnable() {
-                @Override
-                public void run() {
-                    for (int j = 1; j < MAX_TURN; j++) {
-                        Print.cfo(getCurThreadName() + ", 轮次：" + j);
-                    }
-                    Print.cfo(getCurThreadName() + " 运行结束.");
-                }
-            });
-
+            pool.submit(new RunTarget());
         }
         ThreadUtil.sleepMilliSeconds(Integer.MAX_VALUE);
     }
@@ -55,16 +54,7 @@ public class ThreadPoolDemo {
     public void testCpuIntenseTargetThreadPool() {
         ThreadPoolExecutor pool = ThreadUtil.getCpuIntenseTargetThreadPool();
         for (int i = 0; i < 2; i++) {
-            pool.submit(new Runnable() {
-                @Override
-                public void run() {
-                    for (int j = 1; j < MAX_TURN; j++) {
-                        Print.cfo(getCurThreadName() + ", 轮次：" + j);
-                    }
-                    Print.cfo(getCurThreadName() + " 运行结束.");
-                }
-            });
-
+            pool.submit(new RunTarget());
         }
         ThreadUtil.sleepMilliSeconds(Integer.MAX_VALUE);
     }
