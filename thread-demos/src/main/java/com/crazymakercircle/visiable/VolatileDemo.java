e6c0ad4818118ec36a8f29e9dd5067d4e6fa2824
nzcer <ningzhicheng1215@qq.com>
2023-05-01 14:57:10 +0800

    refactor: 完成 4.5-4.7
    - 什么是 Java 内存模型
    - JMM 与 JVM 物理内存的区别
    - JMM 的 8 个操作
    - JMM 如何解决有序性问题
    - volatile 语义中的内存屏障
    - Happens-Before 规则介绍
    - 规则1: 顺序性规则
    - 规则2: volatile 规则
    - 规则3: 传递性规则
    - 规则4: 监视锁规则
    - 规则5: start() 规则
    - 规则6: join() 规则
    - volatile 变量的自增实例
    - volatile 变量的复合操作不具备原子性的原理

--- thread-demos/src/main/java/com/crazymakercircle/visiable/VolatileDemo.java
+++ thread-demos/src/main/java/com/crazymakercircle/visiable/VolatileDemo.java
@@ -2,14 +2,19 @@ package com.crazymakercircle.visiable;
 
 import com.crazymakercircle.util.Print;
 import com.crazymakercircle.util.ThreadUtil;
+import org.junit.Test;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 
+/**
+ * volatile能保证数据的可见性，但volatile不能完全保证数据的原子性，对于volatile类型的变量进行复合操作（如++），其仍存在线程不安全的问题。
+ * volatile变量的复合操作不具备原子性
+ */
 public class VolatileDemo {
     private volatile long value;
 
-    @org.junit.Test
+    @Test
     public void testAtomicLong() {
         // 并发任务数
         final int TASK_AMOUNT = 10;
